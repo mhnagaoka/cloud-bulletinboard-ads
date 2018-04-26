41f46556ba2373f70a504d8716221e56acfbc854
Mauricio Nagaoka <mauricio.nagaoka@sap.com>
2018-04-26 19:10:33 -0300

    Exercise 8, Part 1: Configure Persistence

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -1,14 +1,32 @@
 package com.sap.bulletinboard.ads.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
+@Table(name = "advertisements")
 public class Advertisement {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "mytitle")
     @NotBlank
     private String title;
 
     public String getTitle() {
         return title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setTitle(String title) {
