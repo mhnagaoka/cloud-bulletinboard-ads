919b0071d0b674410d0e8b8c7ca0c4fa1f9fb19d
Mauricio Nagaoka <mauricio.nagaoka@sap.com>
2018-04-27 02:34:24 -0300

    Exercise 11, Step 3: Implement Query Using Criteria Builder

--- src/main/java/com/sap/bulletinboard/ads/models/AdvertisementRepositoryImpl.java
+++ src/main/java/com/sap/bulletinboard/ads/models/AdvertisementRepositoryImpl.java
@@ -3,6 +3,10 @@ package com.sap.bulletinboard.ads.models;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class AdvertisementRepositoryImpl implements AdvertisementRepositoryCustom {
@@ -13,9 +17,16 @@ public class AdvertisementRepositoryImpl implements AdvertisementRepositoryCusto
 
     @Override
     public List<Advertisement> findByTitle(String title) {
-        String qlString = "SELECT ads FROM Advertisement ads WHERE ads.title = :title";
-        TypedQuery<Advertisement> query = entityManager.createQuery(qlString, Advertisement.class);
-        query.setParameter("title", title);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Advertisement> criteriaQuery = criteriaBuilder.createQuery(Advertisement.class);
+        Root<Advertisement> advertisement = criteriaQuery.from(Advertisement.class);
+        ParameterExpression<String> titleParameter = criteriaBuilder.parameter(String.class);
+        criteriaQuery.select(advertisement).where(criteriaBuilder.equal(advertisement.get("title"), titleParameter));
+
+        TypedQuery<Advertisement> query = entityManager.createQuery(criteriaQuery);
+        query.setParameter(titleParameter, title);
+
         return query.getResultList();
     }
 }
